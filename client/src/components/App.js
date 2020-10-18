213e3e8155876384caf458596f828e255947cf92
PMLS3 <peet@partnersinbiz.tech>
2020-10-18 10:44:46 +0200

    user id and local storage

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Login from "./Login";
+import useLocalStorage from "../hooks/useLocalStorage";
+function App() {
+  const [id, setId] = useLocalStorage();
+  return (
+    <>
+      {id}
+      <Login onIdSubmit={setId} />
+    </>
+  );
+}
+
+export default App;
