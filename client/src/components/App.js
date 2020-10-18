74d304b5d8edae5e5de565fd8549749932f16e12
PMLS3 <peet@partnersinbiz.tech>
2020-10-18 13:10:21 +0200

    new contacts modal

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import Login from "./Login";
 import useLocalStorage from "../hooks/useLocalStorage";
+import Dashboard from "./Dashboard";
+import { ContactsProvider } from "../contexts/ContactsProvider";
 function App() {
-  const [id, setId] = useLocalStorage();
-  return (
-    <>
-      {id}
-      <Login onIdSubmit={setId} />
-    </>
+  const [id, setId] = useLocalStorage("id");
+
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id}></Dashboard>
+    </ContactsProvider>
   );
+
+  return id ? dashboard : <Login onIdSubmit={setId} />;
 }
 
 export default App;
